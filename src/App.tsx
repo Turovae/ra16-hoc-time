e3ec249da13f27c496d88ae8ef755efa29e55d62
TurovaE <ekaterina.turova@mail.ru>
2023-06-30 00:22:40 +0700

    add DateTimePretty HOC component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,37 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import moment from "moment";
+
+function getTimePassed(time: string): string {
+  const now = moment();
+  let timeAgo = now.diff(time, "minute");
+
+  if (timeAgo < 60) {
+    return `${timeAgo} минут назад`;
+  }
+
+  timeAgo = now.diff(time, "hour");
+  if (timeAgo < 24) {
+    return `${timeAgo} часов назад`;
+  }
+  console.log(timeAgo);
+  timeAgo = now.diff(time, "day");
+  return timeAgo + " дней назад";
+}
 
 function DateTime(props: { date: string }) {
   return <p className="date">{props.date}</p>;
 }
+
+function DateTimePretty(Component: any, timeFormatter: CallableFunction) {
+  return class extends React.Component {
+    render() {
+      const timeString = timeFormatter(this.props.date);
+      return <Component {...this.props} date={timeString} />;
+    }
+  };
+}
+
+const PrettyTimeComp = DateTimePretty(DateTime, getTimePassed);
 
 function Video(props: { url: string; date: string }) {
   return (
@@ -13,7 +42,7 @@ function Video(props: { url: string; date: string }) {
         allow="autoplay; encrypted-media"
         allowFullScreen
       ></iframe>
-      <DateTime date={props.date} />
+      <PrettyTimeComp date={props.date} />
     </div>
   );
 }
@@ -35,17 +64,20 @@ export default function App() {
     {
       id: 1,
       url: "https://www.youtube.com/embed/rN6nlNC9WQA?rel=0&amp;controls=0&amp;showinfo=0",
-      date: "2017-07-31 13:24:00",
+      // date: "2017-07-31 13:24:00",
+      date: moment().subtract(10, "m").format("YYYY-MM-DD HH:mm:ss"),
     },
     {
       id: 2,
       url: "https://www.youtube.com/embed/dVkK36KOcqs?rel=0&amp;controls=0&amp;showinfo=0",
-      date: "2018-03-03 12:10:00",
+      // date: "2018-03-03 12:10:00",
+      date: moment().subtract(5, "h").format("YYYY-MM-DD HH:mm:ss"),
     },
     {
       id: 3,
       url: "https://www.youtube.com/embed/xGRjCa49C6U?rel=0&amp;controls=0&amp;showinfo=0",
-      date: "2018-02-03 23:16:00",
+      // date: "2018-02-03 23:16:00",
+      date: moment().subtract(6, "d").format("YYYY-MM-DD HH:mm:ss"),
     },
     {
       id: 4,
