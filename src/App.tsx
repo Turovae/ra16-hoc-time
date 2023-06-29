7d9e46187ef5c5dbb8a5a4ff2f049fcc870a3b24
TurovaE <ekaterina.turova@mail.ru>
2023-06-29 22:34:10 +0700

    fix types

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,64 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
-function DateTime(props) {
+function DateTime(props: { date: string }) {
   return <p className="date">{props.date}</p>;
 }
 
-function Video(props) {
+function Video(props: { url: string; date: string }) {
   return (
     <div className="video">
       <iframe
         src={props.url}
-        frameborder="0"
+        frameBorder="0"
         allow="autoplay; encrypted-media"
-        allowfullscreen
+        allowFullScreen
       ></iframe>
       <DateTime date={props.date} />
     </div>
   );
 }
 
-function VideoList(props) {
-  return props.list.map((item) => <Video url={item.url} date={item.date} />);
+function VideoList(props: {
+  list: {
+    id: number;
+    url: string;
+    date: string;
+  }[];
+}) {
+  return props.list.map((item) => (
+    <Video key={item.id} url={item.url} date={item.date} />
+  ));
 }
 
 export default function App() {
-  const [list, setList] = useState([
+  const [list] = useState([
     {
+      id: 1,
       url: "https://www.youtube.com/embed/rN6nlNC9WQA?rel=0&amp;controls=0&amp;showinfo=0",
       date: "2017-07-31 13:24:00",
     },
     {
+      id: 2,
       url: "https://www.youtube.com/embed/dVkK36KOcqs?rel=0&amp;controls=0&amp;showinfo=0",
       date: "2018-03-03 12:10:00",
     },
     {
+      id: 3,
       url: "https://www.youtube.com/embed/xGRjCa49C6U?rel=0&amp;controls=0&amp;showinfo=0",
       date: "2018-02-03 23:16:00",
     },
     {
+      id: 4,
       url: "https://www.youtube.com/embed/RK1K2bCg4J8?rel=0&amp;controls=0&amp;showinfo=0",
       date: "2018-01-03 12:10:00",
     },
     {
+      id: 5,
       url: "https://www.youtube.com/embed/TKmGU77INaM?rel=0&amp;controls=0&amp;showinfo=0",
       date: "2018-01-01 16:17:00",
     },
     {
+      id: 6,
       url: "https://www.youtube.com/embed/TxbE79-1OSI?rel=0&amp;controls=0&amp;showinfo=0",
       date: "2017-12-02 05:24:00",
     },
